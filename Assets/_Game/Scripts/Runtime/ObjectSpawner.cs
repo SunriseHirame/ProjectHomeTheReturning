3369edb692468482f2ab942dc2a6afbe441db8f8
JarmoKu <36786982+JarmoKu@users.noreply.github.com>
2019-01-26 21:34:07 +0200

    Jarmo first branch (#10)
    * Effect practice
    
    * Orbit and attractor

---
+++ Assets/_Game/Scripts/Runtime/ObjectSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject[] Objects;
+    public int Amount;
+
+    private void OnEnable()
+    {
+        for (int i = 0; i < Amount; i++)
+        {
+            int index = Random.Range(0, Objects.Length);
+
+            Vector3 position = new Vector3(Random.Range(-10, 10), 
+                                           Random.Range(-10, 10), 
+                                           Random.Range(-10, 10));
+
+            Instantiate(Objects[index], position, transform.rotation);
+        }
+    }
+}
