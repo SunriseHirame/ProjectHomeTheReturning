0cdef5abe0b41eba4d6bbbe06f7d7c2b4bd46ed0
Bernhard Miettinen <benhard.miettinen@gmail.com>
2019-01-26 16:22:27 +0200

    asd

--- Assets/_Game/Scripts/Runtime/GameManager.cs
+++ Assets/_Game/Scripts/Runtime/GameManager.cs
@@ -7,6 +7,7 @@ namespace Game
 {
     public class GameManager : MonoBehaviour
     {
+        public EssenceTarget[] Target;
         public GlobalValueBase[] Globals;
 
         private void OnEnable ()
@@ -14,6 +15,34 @@ namespace Game
             foreach (var global in Globals)
             {
                 global.Reset ();
+            }
+        }
+
+        private void Update ()
+        {
+            for (var i = 0; i < Target.Length; i++)
+            {
+                if (Target[i].IsCompleted () == false)
+                    return;
+            }
+
+            WinGame ();
+        }
+
+        private void WinGame ()
+        {
+            Debug.Log ("WIN");
+        }
+
+        [System.Serializable]
+        public struct EssenceTarget
+        {
+            public int TargetAmount;
+            public GlobalInt EssenceStore;
+
+            public bool IsCompleted ()
+            {
+                return TargetAmount >= EssenceStore.RuntimeValue;
             }
         }
     }
