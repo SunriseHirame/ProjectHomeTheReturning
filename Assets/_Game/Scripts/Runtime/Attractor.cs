717c853877480503235e5d01535151e4cdeab8fb
JarmoKu <jarmo.august.kuutti@gmail.com>
2019-01-26 18:25:09 +0200

    Orbit and attractor

---
+++ Assets/_Game/Scripts/Runtime/Attractor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attractor : MonoBehaviour
+{
+    public float Orbitals;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<Orbit>())
+        {
+            other.gameObject.GetComponent<Orbit>().StartOrbit();
+            Orbitals++;
+        }
+    }
+}
