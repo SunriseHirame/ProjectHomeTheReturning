17de4e931c6011ab7c31b7d678c241de8b555c56
JarmoKu <jarmo.august.kuutti@gmail.com>
2019-01-26 13:04:24 +0200

    Effect practice

---
+++ Assets/_Game/Scripts/Runtime/Orbit.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    void Update()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.up, 20 * Time.deltaTime);
+    }
+}
