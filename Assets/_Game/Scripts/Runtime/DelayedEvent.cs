54b4e0b1a7eab0251e8e5865488293eeacff1fda
Bernhard Miettinen <benhard.miettinen@gmail.com>
2019-01-27 14:13:00 +0200

    asd

---
+++ Assets/_Game/Scripts/Runtime/DelayedEvent.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Game
+{
+    public class DelayedEvent : MonoBehaviour
+    {
+        public bool FireOnEnable;
+        public float Delay = 1f;
+
+        public UnityEvent Event;
+            
+        private void OnEnable ()
+        {
+            if (FireOnEnable)
+                SendEvent (Delay);
+        }
+
+        public void SendEvent (float delay)
+        {
+            Invoke (nameof (SendEvent), Delay);
+        }
+
+        private void SendEvent ()
+        {
+            Event.Invoke ();
+        }
+    }
+
+}
---
+++ Assets/_Game/Scripts/Runtime/FadeAudioIn.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class FadeAudioIn : MonoBehaviour
+    {
+        public float FadeInLength;
+        public float TargetVolume;
+
+        public AudioSource TargetAudioSource;
+        
+        private void Update ()
+        {
+            TargetAudioSource.volume = Mathf.Lerp (0, TargetVolume, Time.deltaTime / FadeInLength);
+        }
+    }
+
+}
