d2824f91b57c4caea8eab6394a42a2d78f48cb2f
Bernhard Miettinen <benhard.miettinen@gmail.com>
2019-01-27 17:27:26 +0200

    Last minute adjutements

--- Assets/_Game/Scripts/Runtime/Explode.cs
+++ Assets/_Game/Scripts/Runtime/Explode.cs
@@ -1,6 +1,7 @@
-﻿using UnityEditor;
-using UnityEngine;
-
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 namespace Game
 {
     public class Explode : MonoBehaviour
@@ -18,12 +19,14 @@ namespace Game
             }
         }
 
+        #if UNITY_EDITOR
         [ContextMenu ("Find Rigidbodies")]
         private void FindRigidbodies ()
         {
             attachedRigidbodies = GetComponentsInChildren<Rigidbody> ();
             EditorUtility.SetDirty (this);
         }
+        #endif
     }
 
 }
