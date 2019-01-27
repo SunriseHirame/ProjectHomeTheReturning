0aebe29ba73e706614a1947ef1a7e3c78fa9ca09
JarmoKu <36786982+JarmoKu@users.noreply.github.com>
2019-01-27 16:50:09 +0200

    ad (#16)

---
+++ Assets/_Game/Scripts/Runtime/Message.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Message : MonoBehaviour
+{
+    public GameObject[] MessageList;
+    public float time;
+    public float Interval;
+    public int index;
+
+    private void OnEnable()
+    {
+        //index = 0;
+    }
+
+    private void Update()
+    {
+        if (index == 3)
+        {
+            enabled = false;
+            return;
+        }
+
+        time += Time.deltaTime;
+
+        if (time >= Interval)
+        {
+            if (MessageList.Length == index - 1)
+            {
+                enabled = false;
+                return;
+            }
+
+            MessageList[index].SetActive(false);
+            index++;
+            MessageList[index].SetActive(true);
+            time = 0;
+        }
+    }
+}
---
+++ Assets/_Game/Scripts/Runtime/Messages.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Messages : MonoBehaviour
+{
+    public Text text;
+    public CanvasGroup canvasGroup;
+    public float alpha;
+    public float target;
+
+    [TextArea]
+    public string[] MessageList;
+
+    private void OnEnable()
+    {
+        alpha = canvasGroup.alpha;
+        //canvasGroup.alpha = 1;
+        StartCoroutine(ChangeAlpha(1));
+    }
+
+    public void Update()
+    {
+        //canvasGroup.alpha = Mathf.Lerp(alpha, 1, Time.deltaTime);
+    }
+
+    public void NextMessage()
+    {
+
+    }
+
+    public IEnumerator ChangeAlpha(float targetAlpha)
+    {
+        while (alpha != canvasGroup.alpha)
+        {
+            float time = Time.deltaTime;
+            canvasGroup.alpha = Mathf.Lerp(alpha, targetAlpha, time);
+
+            yield return null;
+        }
+    }
+}
